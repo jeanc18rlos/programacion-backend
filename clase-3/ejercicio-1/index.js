8b8e46092c9ba74c29cf4d670e65d999086dd7dd
jean <jeanc16rlos@gmail.com>
2022-01-15 09:56:41 -0400

    first commit

---
+++ clase-3/ejercicio-1/index.js
@@ -0,0 +1,12 @@
+(() => {
+  const hashMap = {};
+  for (let index = 0; index < 1000; index++) {
+    const number = Math.floor(Math.random() * (20 - 0) + 0);
+    if (hashMap[number]) {
+      hashMap[number]++;
+    } else {
+      hashMap[number] = 1;
+    }
+  }
+  console.dir(hashMap);
+})();
